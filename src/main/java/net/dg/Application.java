a8a171865738d750bd92c84c525cc8d8802ae634
dragonSilver <choiye84@gmail.com>
2014-10-28 02:29:47 +0900

    코드 정리

--- src/main/java/net/dg/Application.java
+++ src/main/java/net/dg/Application.java
@@ -3,7 +3,6 @@ package net.dg;
 import com.google.common.collect.Lists;
 import net.dg.domain.movie.*;
 import net.dg.domain.movie.discount.AmountDiscountStrategy;
-import net.dg.domain.movie.discount.PercentDiscountStrategy;
 import net.dg.domain.movie.rule.Rule;
 import net.dg.domain.movie.rule.SequenceRule;
 import net.dg.domain.movie.rule.TimeOfDayRule;
@@ -44,12 +43,9 @@ public class Application {
 
         SequenceRule sequenceRuleOfFirst = new SequenceRule(1);
         SequenceRule sequenceRuleOfSecond = new SequenceRule(2);
-        SequenceRule sequenceRuleOfThird = new SequenceRule(3);
 
         TimeOfDayRule timeOfDayRuleOfFirst = new TimeOfDayRule(DayOfWeek.TUE, TimeInterval.of("T12", "T22"));
         TimeOfDayRule timeOfDayRuleOfSecond =new TimeOfDayRule(DayOfWeek.WED, TimeInterval.of("T18", "T20"));
-        TimeOfDayRule timeOfDayRuleOfThird = new TimeOfDayRule(DayOfWeek.MON, TimeInterval.of("T18", "T20"));
-        TimeOfDayRule timeOfDayRuleOfFourth = new TimeOfDayRule(DayOfWeek.SAT, TimeInterval.of("T18", "T20"));
 
         List<Rule> rulesOfFirst = Lists.newArrayList(sequenceRuleOfFirst, timeOfDayRuleOfFirst);
         AmountDiscountStrategy discountOfFirst = new AmountDiscountStrategy(rulesOfFirst, Money.wons(1000));
@@ -57,19 +53,8 @@ public class Application {
         List<Rule> rulesOfSecond = Lists.newArrayList(sequenceRuleOfSecond, timeOfDayRuleOfSecond);
         AmountDiscountStrategy discountOfSecond = new AmountDiscountStrategy(rulesOfSecond, Money.wons(2000));
 
-
-        List<Rule> rulesOfThird = Lists.newArrayList(sequenceRuleOfThird, timeOfDayRuleOfThird);
-        PercentDiscountStrategy discountOfThird = new PercentDiscountStrategy(10, rulesOfThird);
-
-        List<Rule> rulesOfFourth = Lists.newArrayList();
-        rulesOfFourth.add(timeOfDayRuleOfFourth);
-        PercentDiscountStrategy discountOfFourth = new PercentDiscountStrategy(10, rulesOfFourth);
-
-
         Movie movieOfFirst = movieRepository.save(new Movie("신림프로그래머", 120, Money.wons(20000), discountOfFirst));
         Movie movieOfSecond = movieRepository.save(new Movie("살찐프로그래머", 120, Money.wons(10000), discountOfSecond));
-        Movie movieOfThird = movieRepository.save(new Movie("인천프로그래머", 120, Money.wons(15000), discountOfThird));
-        Movie movieOfFouth = movieRepository.save(new Movie("쿠팡프로그래머", 120, Money.wons(12000), discountOfFourth));
 
         List<DateTimeInterval> dateTimeIntervals =
                 Lists.newArrayList(
@@ -79,7 +64,7 @@ public class Application {
                         DateTimeInterval.of("2014-10-28T19", "2014-10-28T21"),
                         DateTimeInterval.of("2014-10-28T22", "2014-10-29T0") );
 
-        for(Movie eachMovie : Lists.newArrayList(movieOfFirst, movieOfSecond, movieOfThird, movieOfFouth)) {
+        for(Movie eachMovie : Lists.newArrayList(movieOfFirst, movieOfSecond)) {
             int sequence = 1;
             for(DateTimeInterval eachInterval : dateTimeIntervals) {
                 showingRepository.save(new Showing(eachMovie, sequence++, eachInterval));
